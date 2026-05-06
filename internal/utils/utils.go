57ea82fe78f175eac80f682c9ae9f0c19ca88ae1
edu292 <eduskoroboatei@gmail.com>
2026-05-06 16:37:41 -0300

    codecrafters submit [skip ci]

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,11 +3,9 @@ package utils
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
-	"time"
 	"unicode"
 
 	"golang.org/x/sys/unix"
@@ -74,11 +72,6 @@ func ParseInput(input []byte) (string, []string) {
 	var startParseIdx int
 	var idx int
 	for {
-		fmt.Println(startParseIdx)
-		fmt.Println(idx)
-		fmt.Println(args)
-		fmt.Println()
-		time.Sleep(200 * time.Millisecond)
 		startParseIdx = bytes.IndexFunc(byteArgs[idx:], func(r rune) bool {
 			return !unicode.IsSpace(r)
 		})
@@ -107,9 +100,5 @@ func ParseInput(input []byte) (string, []string) {
 
 		args = append(args, argByteToString(byteArgs[startParseIdx:idx]))
 	}
-	fmt.Println(startParseIdx)
-	fmt.Println(idx)
-	fmt.Println(args)
-
 	return cmd, args
 }
