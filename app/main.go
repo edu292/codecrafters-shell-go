acd5e05a210d4c2a26ba34c7853b99a1f109c70a
edu292 <eduskoroboatei@gmail.com>
2026-05-06 10:42:48 -0300

    Type

--- app/main.go
+++ app/main.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
 )
+
+var errExit = errors.New("exit")
+
+type command func(args []string) error
+
+func exit(args []string) error {
+	return errExit
+}
+
+func echo(args []string) error {
+	fmt.Println(strings.Join(args, " "))
+
+	return nil
+}
+
+func typeHandler(args []string) error {
+	cmd := args[0]
+	if _, exists := commands[cmd]; !exists {
+		fmt.Printf("%s: invalid_command", cmd)
+		return nil
+	}
+
+	fmt.Printf("%s is a shell buitin", cmd)
+	return nil
+}
+
+var commands = make(map[string]command)
+
+func init() {
+	commands["exit"] = exit
+	commands["echo"] = echo
+	commands["type"] = typeHandler
+}
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -15,15 +49,23 @@ outer:
 		if !scanner.Scan() {
 			break
 		}
+
 		fields := strings.Fields(scanner.Text())
 		cmd, args := fields[0], fields[1:]
-		switch cmd {
-		case "exit":
-			break outer
-		case "echo":
-			fmt.Println(strings.Join(args, " "))
-		default:
+
+		fn, exists := commands[cmd]
+		if !exists {
 			fmt.Printf("%s: command not found\n", cmd)
+			continue
+		}
+
+		err := fn(args)
+		if err == errExit {
+			break outer
+		}
+
+		if err != nil {
+			fmt.Printf("%v", err)
 		}
 	}
 }
